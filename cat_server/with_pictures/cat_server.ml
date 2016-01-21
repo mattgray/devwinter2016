1401a25211375ff0903e786f8423e48256c474bb
Matt Gray <mgray@fastly.com>
2016-01-21 22:32:39 +0000

    Cats

---
+++ cat_server/with_pictures/cat_server.ml
@@ -0,0 +1,24 @@
+open Lwt
+open V1_LWT
+
+module Main
+  (C:CONSOLE)
+  (S: Cohttp_lwt.Server)
+  (FS: KV_RO)
+  = struct
+
+  let start console http pics =
+    let read_fs name =
+      FS.size pics name >>= function
+      | `Error (FS.Unknown_key _) -> fail (Failure ("read " ^ name))
+      | `Ok size ->
+        FS.read pics name 0 (Int64.to_int size) >>= function
+        | `Error (FS.Unknown_key _) -> fail (Failure ("read " ^ name))
+        | `Ok bufs -> return (Cstruct.copyv bufs)
+    in
+
+    let handle_request conn_id request body =
+      read_fs "cats0.jpg" >>= fun pic -> S.respond_string ~status: `OK ~body:pic ()
+    in
+    http (`TCP 8080) (S.make ~callback:handle_request ())
+end
